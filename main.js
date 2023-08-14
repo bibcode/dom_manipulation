04063d5781a4da0b84d6120d616b665d574af551
bibcode <bibcodes5@gmail.com>
2023-08-14 14:56:19 +0100

    updated project

--- main.js
+++ main.js
@@ -2,68 +2,90 @@ const books = [
     {
         id: 1,
         name: "Wonder",
-        category: "fiction",
+        category: "Fiction",
+        price:"$60.99",
         image: "IMG/Screenshot 2023-07-27 163136.png"
     },
 
     {
         id: 2,
         name: "Who moved my cheese",
-        category: "fiction",
+        category: "Fiction",
+        price:"$99.99",
         image: "IMG/Screenshot 2023-07-27 163317.png"
     },
     {
         id: 3,
         name: "Sooley",
-        category: "novels",
+        category: "Novels",
+        price:"$70.99",
         image: "IMG/Screenshot 2023-07-27 163432.png"
     },
     {
         id: 4,
         name: "My Wonder",
-        category: "adventure",
+        category: "Adventure",
+        price:"$30.99",
         image: "IMG/Screenshot 2023-07-27 163136.png"
     },
     {
         id: 5,
         name: "My brilliant freind",
-        category: "novels",
+        category: "Novels",
+        price:"$40.99",
         image: "IMG/Screenshot 2023-07-27 163336.png"
     },
     {
         id: 6,
         name: "Sparring Partners",
-        category: "adventure",
+        category: "Adventure",
+        price:"$80.99",
         image: "IMG/Screenshot 2023-07-27 163406.png"
     }
 ]
-document.getElementById("demo")
-function fiction() {
-    const myFiction = books.filter((book) => {
-        return book.category.includes("fiction")
-    })
-    demo.innerHTML = myFiction;
 
-}
-document.getElementById("demo")
-function adventure() {
-    const myAdventure = books.filter((book) => {
-        return book.category.includes("adventure")
+function handleFilter(event) {
+    console.log("event>>", event.target.textContent);
+    document.getElementById("demo").innerHTML = "";
+    const filterdBooks = books.filter((book) => {
+        return book.category.includes(event.target.textContent)
     })
-    demo.innerHTML = myAdventure;
-
-}
-document.getElementById("demo")
-function novels() {
-    const myNovels = books.filter((book) => {
-        return book.category.includes("fiction")
+    console.log(filterdBooks);
+    let bookContent = filterdBooks.map((book) => {
+        let div =
+            `<div>
+          <p>${book.id}</p>
+             <img src="${book.image}">
+            <p>${book.name}</p>
+             <button id="btn">Add to cart</button> 
+            </div>`
+        document.getElementById("demo").innerHTML += div;
     })
-    demo.innerHTML = myNovels;
-
 }
 
+function bookPrice() {
+    document.getElementById("demo").innerHTML ="";
+    const priceFilter =books.filter((book)=>{
+        return book.price < 60.99;
+    })
+    console.log(priceFilter);
+    let bookContent = priceFilter.map((book) => {
+        let div =
+            `<div>
+          <p>${book.id}</p>
+             <img src="${book.image}">
+            <p>${book.name}</p>
+             <button id="btn">Add to cart</button> 
+            </div>`
+        document.getElementById("demo").innerHTML += div;
+    })
+}
 
-let demo = document.getElementById("demo")
+
+
+
+
+
 
 let bookElement = books.map((book) => {
 
@@ -72,25 +94,9 @@ let bookElement = books.map((book) => {
              <p>${book.id}</p>
              <img src="${book.image}">
             <p>${book.name}</p>
+            <p>${book.price}</p>
              <button id="btn">Add to cart</button> 
             </div>`
-    demo.innerHTML += div
+    document.getElementById("demo").innerHTML += div
 
 })
-
-
-
-
-// function novels() {
-//     const myNovels = books.filter((book) => {
-//         return book.category.includes("novels")
-//     })
-//     return myNovels
-// }
-// function adventure() {
-//     const myAdventure = books.filter((book) => {
-//         return book.category.includes("adventure")
-//     })
-//     return myAdventure
-
-// }
